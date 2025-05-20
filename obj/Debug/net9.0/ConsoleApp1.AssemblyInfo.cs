5700086ee1fa5c47a89436c20fe1ad29676b4a5a
Asadi1385 <faeze1385asadi@gmail.com>
2025-05-20 20:04:10 +0330

    faeze gozashtam

--- obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da291b5b25f1f397c35c9bb4e2f86d4e989cdac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b697e5de53101c5a5028c2e8b81816206ef8a8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
