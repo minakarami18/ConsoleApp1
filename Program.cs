a17df8ca8de581942f705289d03438379318bd04
minakarami18 <minakaraminejad84@gmail.com>
2025-05-20 14:45:22 +0330

    new

--- Program.cs
+++ Program.cs
@@ -4,12 +4,6 @@ public class Program
 {
     public static void Main()
     {
-<<<<<<< HEAD
-        console.writline("مینا خره");
-=======
-        Console.WriteLine("mهدش ناشقث");
-
->>>>>>> 5a554842f2e2828e9130a70870dc4c9925696cf7
 
     }
 }
